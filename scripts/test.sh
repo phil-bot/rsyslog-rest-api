84d0a23930bdf4d7c0ce5c7dce039155f4359517
phil <github@der-ju.de>
2026-02-24 19:49:05 +0100

    mod: test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,8 @@
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 cd "$REPO_DIR" || exit 1
-
 DOCKER_DIR="docker"
-
-make all
-
+[[ $1 == "make" ]] && make all
 cd "$DOCKER_DIR" || exit 1
-
 docker compose restart
-#docker exec -it rsyslox-test /opt/rsyslox/log-generator.sh
 
